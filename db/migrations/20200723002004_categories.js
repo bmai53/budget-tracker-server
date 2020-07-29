124aff99e7809396b1c6f974ff693f6268296fa3
bmai <bmai@sfu.ca>
2020-07-28 23:58:49 -0700

    fixed migration tables

--- db/migrations/20200723002004_categories.js
+++ db/migrations/20200723002004_categories.js
@@ -1,7 +1,7 @@
 
 exports.up = function(knex) {
     return knex.schema.createTable("categories", (table) => {
-        table.increments('category_id')
+        table.increments('id')
         table.integer('user_id').notNullable()
         table.string('name', 255).notNullable()
     })
