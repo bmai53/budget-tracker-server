ce0aa467b5bcb7cec23c8731781f879e9ba22bd7
bmai <bmai@sfu.ca>
2020-07-30 04:17:15 -0700

    added activity seed

--- db/migrations/20200729003333_activities.js
+++ db/migrations/20200729003333_activities.js
@@ -3,9 +3,9 @@ exports.up = function (knex) {
     return knex.schema.createTable("activities", (table) => {
         table.increments('id')
         table.integer('user_id').notNullable()
-        table.foreign('user_id').references('users.id').onDelete('cascade');
+        table.foreign('user_id').references('id').inTable('users').onDelete('cascade');
         table.integer('category_id').notNullable()
-        table.foreign('category_id').references('categories.id').onDelete('cascade');
+        table.foreign('category_id').references('id').inTable('categories').onDelete('cascade');
         table.string('name', 255).notNullable()
         table.decimal('amount').notNullable()
     })
