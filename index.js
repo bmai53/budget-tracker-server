07858e040048fc32377b275a5fe070774fad43cf
bmai <bmai@sfu.ca>
2020-07-30 05:50:43 -0700

    removed x powered by

--- index.js
+++ index.js
@@ -7,16 +7,12 @@ const port = process.env.PORT || 5000
 // Parsing data
 app.use(Express.json())
 
+// remove x-powered-by Express
+app.disable('x-powered-by');
+
 // cors
 const cors = require('cors')
 app.use(cors())
-
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, Accept');
-    next();
-});
 
 // passport
 const passport = require('passport')
